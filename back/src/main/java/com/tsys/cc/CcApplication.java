6a0fcb29612c6b9a11accc280e3238f05c84555f
Arpad Balogh <arpad.balogh@t-systems.com>
2018-09-04 13:56:25 +0200

    test

---
+++ back/src/main/java/com/tsys/cc/CcApplication.java
@@ -0,0 +1,12 @@
+package com.tsys.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcApplication.class, args);
+	}
+}
